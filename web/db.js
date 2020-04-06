7fee7f1f44b54d0d23875cfe334119341e0085b6
aralaa <90aralala@gmail.com>
2020-04-06 22:08:41 +0900

    web server

---
+++ web/db.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+
+var pool;
+
+exports.connect = function(done) {
+    pool = mysql.createPool({
+        connectionLimit: 100,
+        host:'localhost',
+        port: 3306,
+        user:'root',
+        password: 'mysql1234',
+        database: 'bookjounaldb',
+    });
+}
+
+exports.get = function() {
+  return pool;
+}
