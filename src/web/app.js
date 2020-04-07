04c713a930d883233e9a7b819c7972a4101aab19
aralaa <90aralala@gmail.com>
2020-04-07 14:23:03 +0900

    코드 수정

--- src/web/app.js
+++ src/web/app.js
@@ -7,7 +7,7 @@ var db = require('./db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var bookjounalRouter = require('./routes/bookjounal');
+var bookjournalsRouter = require('./routes/bookjournals');
 
 var app = express();
 
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bookjounal', bookjounalRouter);
+app.use('/bookjournals', bookjournalsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
